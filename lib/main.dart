3d84bda9ed12d0087fa1df9ade893a48ac64abfb
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-06 15:21:33 -0500

    étape 3 ok

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
             children: [
               Text('Hello World!'),
-                Text('Hello Belka!')
+              Text('Hello Belka!'),
+              Container(
+                margin :EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text("boite stylisée",
+                style: TextStyle(color: Colors.deepOrangeAccent,fontSize: 24),
+              )),
+               Container(
+                margin :EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Text("Ma 2e boite stylisée",
+                style: TextStyle(color: Colors.black,fontSize: 44),
+                )),
             ],
           ),
         ),
