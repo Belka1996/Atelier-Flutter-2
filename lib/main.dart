47a23797a3247a0c5a3c88c8e1b37bdc22ebd996
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-06 15:40:25 -0500

    étape 4 ok

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,15 @@ class MainApp extends StatelessWidget {
                 child: Text("Ma 2e boite stylisée",
                 style: TextStyle(color: Colors.black,fontSize: 44),
                 )),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(width: 200, height: 200, color: Colors.blue[100]),
+                    Icon(Icons.star, size: 100, color: Colors.amber),
+                    Text("Superposé !",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )
             ],
           ),
         ),
